6532e41b97e1090ecc04706facdef954a8ff3889
Ahmaud R. Templeton <ahmaud@gmail.com>
2021-01-19 18:20:23 -0500

    commit tree d7db4496eb604b5dad2d4337599e0362aaf70a02

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
 # Write your code here.
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+    badges = []
+    attendees.each { |attendee| badges << "Hello, my name is #{attendee}."}
+    badges
+end
+
+def assign_rooms(attendees)
+    assigned = []
+    attendees.each.with_index(1) { |name, room| assigned  << "Hello, #{name}! You'll be assigned to room #{room}!" }
+    return assigned
+end
+
+def printer(attendees)
+    batch_badge_creator(attendees).each do |output|
+        puts "#{output}"
+    end
+    assign_rooms(attendees).each do |output|
+        puts "#{output}"
+    end
+end
